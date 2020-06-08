d9efde6688541b1741695013a2a58b3a82aea33f
Shirakami Emmmer <yjhdandan@163.com>
2020-06-08 22:57:53 +0800

    add black

--- assignment/3/danmaku-management-py/main.py
+++ assignment/3/danmaku-management-py/main.py
@@ -3,10 +3,13 @@
 
 import asyncio
 import json
+import os
+
 import requests
 from ki_logger import Logger
 import blivedm
 import cookies as cookies_getter
+
 
 async def set_block_user(room_id, csrf, user_uid):
     block_url = "https://api.live.bilibili.com/banned_service/v2/Silent/add_block_user"
@@ -25,9 +28,15 @@ async def judge(danmaku_content):
 class MyBLiveClient(blivedm.BLiveClient):
     # 演示如何自定义handler
     _COMMAND_HANDLERS = blivedm.BLiveClient._COMMAND_HANDLERS.copy()
+    repeat_list = []
 
     async def _on_receive_danmaku(self, danmaku: blivedm.DanmakuMessage):
+
         if danmaku.msg_type == 0:
+            if await judge(danmaku.msg):
+                mes = await set_block_user(room, my_csrf, danmaku.uid)
+                if mes['code'] == 0:
+                    Logger.log(f"已禁言: {danmaku.uname} 他的uid为: {danmaku.uid} 他的弹幕为: {danmaku.msg}")
             Logger.log(f"{danmaku.timestamp}:{danmaku.uid}:{danmaku.msg}")
 
 
@@ -44,10 +53,17 @@ async def main():
 
 
 if __name__ == '__main__':
-    room = 763869
+    room = int(input("请输入房间号："))
+    Logger.init(9999)
+    Logger.log("程序将请求您的cookies信息，请允许")
     cookies = cookies_getter.get_bilibili_cookies()
     my_csrf = cookies['bili_jct']
-    black = {}
-    Logger.init(9999)
+    black = []
+    if os.path.exists("./black.txt"):
+        file = open("./black.txt", "r")
+        for line in file:
+            line = line.split()[0]
+            if line:
+                black.append(line)
+        file.close()
     asyncio.get_event_loop().run_until_complete(main())
-
